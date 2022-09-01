f9bd684f41dd2b964aedd044cf75b1bacda5169f
Mohamed magdy <56060941+MagdyEG@users.noreply.github.com>
2022-09-01 13:02:06 +0200

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,35 @@
+from operator import index
+import re
+
+def reverse_string(string):
+     return string[::-1]
+
+
+
+def reverse5(mystr):
+ x = l1.split() 
+ for i in range(len(x)):
+    j = x[i]
+    if len(j[i]) < 4 :
+        print(x[::-1])
+    else :
+        print(j[i].reverse())
+
+
+
+
+l2 = "ahmed gg magdy mohameddd"
+#print(reverse_string(l2))  
+
+
+
+
+
+
+l1 = "ahmed gg magdy moahemdggmagdy"  
+x = l1.split()
+for i in range(len(x)):
+    print(x[i])
+    print(reverse_string(x[i]))
+
+
